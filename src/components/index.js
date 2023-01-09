2819f78c6757c3be89c3623e2f56817c724b7025
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-09 13:35:36 +0400

    feat: ✨Ajout d'un index.js dans les composants pour un export commun des composants MUI.

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Checkbox from '@mui/material/Checkbox'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import { DialogContent } from '@mui/material'
+
+export {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  DialogContent,
+  Grid,
+  Link,
+  LockOutlinedIcon,
+  FormControlLabel,
+  Paper,
+  TextField,
+  Typography,
+}
