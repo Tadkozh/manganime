063125c0b7db56f8d274da0a3f1c7c64a0ae230e
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-09 19:21:57 +0400

    feat: ✨ Mise en place de la route restreinte de UserProfile

--- src/AppConsumer.js
+++ src/AppConsumer.js
@@ -4,6 +4,8 @@ import { ErrorFallback } from './components/ErrorFallBack'
 import { MangAnime } from './components/MangAnime'
 import { LoginRegister } from './components/LoginRegister'
 import { Error404 } from './components/Error404'
+import { UserProfile } from './components/UserProfile'
+import { PrivateRoute } from './components/PrivateRoute'
 
 const AppConsumer = () => {
   return (
@@ -13,6 +15,14 @@ const AppConsumer = () => {
         <Route path="/" element={<MangAnime />} />
         <Route path="/login" element={<LoginRegister />} />
         <Route path="*" element={<Error404 />} />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <UserProfile />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   )
