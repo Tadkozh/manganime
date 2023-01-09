ec7dd96b53d310a42f82014810fbef92b8562222
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-09 10:41:37 +0400

    feat: ✨Mise en place de React-query

--- src/context/index.js
+++ src/context/index.js
@@ -1,7 +1,34 @@
 import { AuthProviders } from './AuthContext'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ENV_DEV } from '../commons/constants'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retryDelay: 500,
+    retry: (failureCount, error) => {
+      if (error.status === 404) return false
+      else if (error.status === 401) return false
+      else if (failureCount > 3) return false
+      else return true
+    },
+    mutations: {
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      retryDelay: 500,
+      retry: 1,
+    },
+  },
+})
 
 const AppProviders = ({ children }) => {
-  return <AuthProviders>{children}</AuthProviders>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProviders>{children}</AuthProviders>
+      {process.env.NODE_ENV === ENV_DEV && <div></div>}
+    </QueryClientProvider>
+  )
 }
 
 export { AppProviders }
