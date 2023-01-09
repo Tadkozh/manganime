412af7d1b204f7d5105a3bb1e788989bfec45e5c
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-09 13:31:53 +0400

    feat: ✨Mise en place d'un test LoginRegister et la liste des tests a réalisés.

--- src/components/__tests__/LoginRegister.test.js
+++ src/components/__tests__/LoginRegister.test.js
@@ -2,20 +2,57 @@ import { render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { LoginRegister } from '../LoginRegister'
 import { screen } from '@testing-library/react'
+import { faker } from '@faker-js/faker'
 
+describe('LoginRegister component', () => {
+  test('Quand les champs sont correctement renseigné, on accède à la page utilisateur', async () => {
+    const user = userEvent.setup()
+    render(<LoginRegister />)
 
-// Quand je clique sur le bouton après avoir rempli le formulaire, je me connecte
-test('Quand les champs sont rempli, on devrais', async () => {
-  const user = userEvent.setup()
-  render(<LoginRegister />)
+    const inputEmail = await screen.findByRole('textbox', {
+      name: /adresse email/i,
+    })
+    const inputPassword = await screen.findByRole('textbox', {
+      name: /mot de passe/i,
+    })
 
-  const connexionButton = await screen.findByRole('button', {
-    name: /Se connecter/i,
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+
+    await user.type(inputEmail, email)
+    await user.type(inputPassword, password)
+
+    const connexionButton = await screen.findByRole('button', {
+      name: /connexion/i,
+    })
+
+    await user.click(connexionButton)
+    // await expect(
+    //   screen.findByRole('heading', { name: /AuthApp/i }),
+    // ).resolves.toBeInTheDocument()
   })
-
-  // screen.debug()
-  user.click(connexionButton)
-  // await expect(
-  //   screen.findByRole('heading', { name: /AuthApp/i }),
-  // ).resolves.toBeInTheDocument()
+  test.todo(
+    `Quand on clique sur le lien 'créer un compte', le header et le texte du boutton change`,
+    async () => {},
+  )
+  test.todo(
+    `Quand l'email est vide, on obtient un message d'erreur`,
+    async () => {},
+  )
+  test.todo(
+    `Quand le mot de passe est vide, on obtient un message d'erreur`,
+    async () => {},
+  )
+  test.todo(
+    `Quand le champs email est un incorrect, on obtient un message d'erreur`,
+    async () => {},
+  )
+  test.todo(
+    `Quand le champs mot de passe est un incorrect, on obtient un message d'erreur`,
+    async () => {},
+  )
+  test.todo(
+    `Quand la longeur du mot de passe est inférieur à six, on obtient un message d'erreur`,
+    async () => {},
+  )
 })
