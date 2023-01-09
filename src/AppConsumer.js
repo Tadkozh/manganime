1f05e1af6fe909e41b31568afd74e6792de173b6
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-09 11:59:02 +0400

    feat: ✨Mise en place de la route pour la home page.

--- src/AppConsumer.js
+++ src/AppConsumer.js
@@ -1,12 +1,15 @@
 import { ErrorBoundary } from 'react-error-boundary'
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ErrorFallback } from './components/ErrorFallBack'
+import { MangAnime } from './components/MangAnime'
 
 const AppConsumer = () => {
   return (
     <Router>
       <ErrorBoundary FallbackComponent={ErrorFallback}></ErrorBoundary>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<MangAnime />} />
+      </Routes>
     </Router>
   )
 }
