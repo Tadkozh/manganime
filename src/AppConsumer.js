9d98f226d789fcef2ce9fe3f3e2fa4b031ce49f5
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-09 11:57:20 +0400

    feat: ✨Mise en place de la gestion d'erreur sur l'application.

--- src/AppConsumer.js
+++ src/AppConsumer.js
@@ -1,8 +1,11 @@
+import { ErrorBoundary } from 'react-error-boundary'
 import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { ErrorFallback } from './components/ErrorFallBack'
 
 const AppConsumer = () => {
   return (
     <Router>
+      <ErrorBoundary FallbackComponent={ErrorFallback}></ErrorBoundary>
       <Routes></Routes>
     </Router>
   )
