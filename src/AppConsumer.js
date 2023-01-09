212803df56e63140a9f76c125a426a2113f4936c
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-09 11:53:21 +0400

    feat: ✨Création et mise en place du routage

---
+++ src/AppConsumer.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
+
+const AppConsumer = () => {
+  return (
+    <Router>
+      <Routes></Routes>
+    </Router>
+  )
+}
+
+export default AppConsumer
