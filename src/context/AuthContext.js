f70d5d3963be25c618efcf794a5e17af5d9aa75f
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-10 22:51:12 +0400

    refactor: 🎨refactorisation de AuthContext avec utilisation de hook perso pour gérer les données.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,100 +1,56 @@
 import React from 'react'
+
 import {
-  USER_NOT_FOUND,
-  WRONG_PASSWORD,
-  TOO_MANY_REQUEST,
-} from '../utils/constants'
-import {
-  BAD_USE_CONTEXT,
-  AUTH_USER_NOT_FOUND,
-  AUTH_WRONG_PASSWORD,
-  AUTH_TOO_MANY_REQUEST,
-} from '../commons/constants'
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
+  browserSessionPersistence, createUserWithEmailAndPassword, setPersistence, signInWithEmailAndPassword,
+  signOut
 } from 'firebase/auth'
+import { BAD_USE_CONTEXT, SIGN_IN } from '../commons/constants'
 import { auth } from '../firebase-config'
+import { useUserData } from '../hooks/useUserData'
 import { validateForm } from '../utils/helper'
-import { useCleanupError } from '../hooks/cleanupError'
 
 const AuthContext = React.createContext()
 
-const errorAuth = (error) => {
-  let message
-  switch (error.code) {
-    case AUTH_USER_NOT_FOUND:
-      message = USER_NOT_FOUND
-      break
-    case AUTH_WRONG_PASSWORD:
-      message = WRONG_PASSWORD
-      break
-    case AUTH_TOO_MANY_REQUEST:
-      message = TOO_MANY_REQUEST
-      break
-    default:
-      message = error.message
-      break
-  }
-  const newError = {
-    code: 400,
-    message: message,
-  }
-  return newError
-}
-
 const AuthProviders = (props) => {
-  const [data, setData] = React.useState(null)
-  const [status, setStatus] = React.useState(false)
-  const { clean: error, setClean: setError } = useCleanupError(null)
+  const { data, status, error, setError, execute, setData } = useUserData()
 
   const register = React.useCallback(
     (email, password) => {
-      const errorForm = validateForm(email, password)
-      if (!errorForm) {
-        setStatus(true)
-        createUserWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => setData(userCredential.user))
-          .catch((err) => {
-            setError(err)
-          })
-        setStatus(false)
-      } else {
-        setError(errorForm)
-      }
+      setPersistence(auth, browserSessionPersistence).then(() => {
+        execute(createUserWithEmailAndPassword(auth, email, password))
+      })
     },
-    [setError],
+    [execute],
   )
   const login = React.useCallback(
     (email, password) => {
-      const errorForm = validateForm(email, password)
-      if (!errorForm) {
-        setStatus(true)
-        signInWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            setData(userCredential.user)
-          })
-          .catch((err) => {
-            const newError = errorAuth(err)
-            setError(newError)
-          })
-        setStatus(false)
-      } else {
-        setError(errorForm)
-      }
+      setPersistence(auth, browserSessionPersistence).then(() => {
+        execute(signInWithEmailAndPassword(auth, email, password))
+      })
     },
-    [setError],
+    [execute],
   )
-  const logout = () => {
+  const preValidate = React.useCallback(
+    (email, password, action = SIGN_IN) => {
+      const errorForm = validateForm(email, password)
+      if (errorForm) {
+        setError(errorForm)
+        return
+      }
+      action === SIGN_IN ? login(email, password) : register(email, password)
+    },
+    [login, register, setError],
+  )
+
+  const logout = React.useCallback(() => {
     signOut(auth).then(() => {
       setData(null)
     })
-  }
+  }, [setData])
 
   const values = React.useMemo(
-    () => ({ login, logout, register, data, error, status }),
-    [data, error, login, register, status],
+    () => ({ preValidate, logout, data, error, status }),
+    [data, error, logout, preValidate, status],
   )
   return <AuthContext.Provider {...props} value={values} />
 }
@@ -108,3 +64,4 @@ const useAuth = () => {
 }
 
 export { AuthProviders, useAuth }
+
