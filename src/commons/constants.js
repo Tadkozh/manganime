791db2ccd325691184d568e7115b2e8e1c6d0cc2
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-10 13:52:38 +0400

    feat: ✨ Ajout des erreurs d'authentification lors de la connexion.

--- src/commons/constants.js
+++ src/commons/constants.js
@@ -1,4 +1,13 @@
 const BAD_USE_CONTEXT = 'ne peut pas être utilisé sans'
 const ENV_DEV = 'development'
+const AUTH_USER_NOT_FOUND = 'auth/user-not-found'
+const AUTH_WRONG_PASSWORD = 'auth/wrong-password'
+const AUTH_TOO_MANY_REQUEST = 'auth/too-many-requests'
 
-export { BAD_USE_CONTEXT, ENV_DEV }
+export {
+  BAD_USE_CONTEXT,
+  ENV_DEV,
+  AUTH_USER_NOT_FOUND,
+  AUTH_WRONG_PASSWORD,
+  AUTH_TOO_MANY_REQUEST,
+}
