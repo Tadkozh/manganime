dfac60d2074339aa56150f478e005f0cb1f135ab
Ponifty <ponifty@gmail.com>
2023-01-11 18:30:39 +0100

    🎨 top hook refacto

--- src/components/TopDetails.js
+++ src/components/TopDetails.js
@@ -1,30 +1,14 @@
 import React from 'react'
 import useGetTopDatas from '../hooks/getTopDatas'
-import SearchTop from './SearchTop'
 import TopView from './TopView'
 
 const TopDetails = ({ name }) => {
-  const topAnimesApiUrl = 'https://api.jikan.moe/v4/top/anime'
-  const topMangasApiUrl = 'https://api.jikan.moe/v4/top/manga'
-  let url = ''
-  const [isSubmitted, setisSubmitted] = React.useState(false)
-  const [topDatas, setTopDatas] = React.useState([])
-
-  if (name === 'Animes') {
-    url = topAnimesApiUrl
-  } else if (name === 'Mangas') {
-    url = topMangasApiUrl
-  } else {
-    throw new Error('Invalid name')
-  }
-
-  useGetTopDatas(isSubmitted, url, setTopDatas, setisSubmitted)
+  const topDatas = useGetTopDatas(name)
 
   return (
     <>
       <article>
-        <h2>Top {name}</h2>
-        <SearchTop name={name} handleSubmit={(e) => setisSubmitted(true)} />
+        <h2>Top {name.toUpperCase()}</h2>
         <TopView datas={topDatas} />
       </article>
     </>
