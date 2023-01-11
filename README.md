050141e4012d6d09a5bb7d0017d7fbf6025ab436
Tadkozh <tad.kozh.bihan@free.fr>
2023-01-11 19:09:39 +0100

    update root README.md

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Projet React Mastery - Anime
+# Projet React Mastery - Anime | Manga
+
+**Outils utilisés :**
+
+- Notion
+- Trello
+- Git-
+- Github
+- VSCode
+- npm
+
+**Libraries :**
+
+- Style JS :
+
+  - prettier
+
+- Style CSS :
+
+  - Material UI
+
+- Backend :
+
+  - Firebase (Authentication + Firestore Database)
+
+- React :
+
+  - axios
+  - react-router-dom
+  - react-error-boundary
+  - react-query
+
+- Tests :
+  - msw
+  - faker
+  - Jest
+  - Cypress
+  - user-event
+  - React Testing Library
--- src/components/NewsAnime.jsx
+++ src/components/NewsAnime.jsx
@@ -13,11 +13,12 @@ export const NewsAnime = () => {
   const [animeNews, setAnimeNews] = useState([])
 
   // https://api.jikan.moe/v4/anime/${id}/news
+  //https://api.jikan.moe/v4/manga/{id}/news
 
   const APP_API_URL = 'https://api.jikan.moe/v4'
   const endpoint = 'news'
   const id = 1 // id : 1, 190
-  const params = 'manga' // params : anime, manga
+  const params = 'anime' // params : anime, manga
 
   // const clientApi = (endpoint = null, params = {}) => {
   //   return axios
@@ -44,7 +45,7 @@ export const NewsAnime = () => {
 
   return (
     <>
-      <h2>News about this ${params}</h2>
+      <h2>News about this {params}</h2>
       <p>(click on a title to learn more)</p>
       <div>
         {animeNews
