1684b1b6edf1e00c72d0b1051ec398fade54be8d
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-11 10:31:31 +0400

    refactor: 🎨Changement de fichier pour la fonction errorAuth.

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,8 +1,20 @@
 import {
+  AUTH_EMAIL_EXISTS,
+  AUTH_TOO_MANY_REQUEST,
+  AUTH_USER_ALREADY_EXIST,
+  AUTH_USER_NOT_FOUND,
+  AUTH_WRONG_PASSWORD
+} from '../commons/constants'
+import {
+  EMAIL_EXIST,
   EMAIL_NOT_VALID,
   EMAIL_REQUIRED,
   PASSWORD_REQUIRED,
   PASSWORD_REQUIREMENT,
+  TOO_MANY_REQUEST,
+  USER_EXSIT,
+  USER_NOT_FOUND,
+  WRONG_PASSWORD
 } from './constants'
 
 const validateForm = (email, password) => {
@@ -33,4 +45,33 @@ const validateForm = (email, password) => {
   return null
 }
 
-export { validateForm }
+const errorAuth = (error) => {
+  let message
+  switch (error.code) {
+    case AUTH_USER_NOT_FOUND:
+      message = USER_NOT_FOUND
+      break
+    case AUTH_WRONG_PASSWORD:
+      message = WRONG_PASSWORD
+      break
+    case AUTH_TOO_MANY_REQUEST:
+      message = TOO_MANY_REQUEST
+      break
+    case AUTH_USER_ALREADY_EXIST:
+      message = USER_EXSIT
+      break
+    case AUTH_EMAIL_EXISTS:
+      message = EMAIL_EXIST
+      break
+    default:
+      message = error.message
+      break
+  }
+  const newError = {
+    code: 400,
+    message: message,
+  }
+  return newError
+}
+
+export { validateForm, errorAuth }
