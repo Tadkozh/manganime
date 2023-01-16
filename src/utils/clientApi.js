d98f728a3f86dd067e1790c052470e3135a7f1bc
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-16 11:21:29 +0400

    feat: ✨Créatiion de clientApi pour les appels api

---
+++ src/utils/clientApi.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import { JIKAN_API } from '../commons/constants'
+
+const clientApi = async (endpoint) => {
+  console.log('REQUEST', `${JIKAN_API}/${endpoint}`)
+  return await axios
+    .get(`${JIKAN_API}/${endpoint}`)
+    .then((data) => data?.data)
+    .catch((error) => {
+      if (error.response) {
+        const err = {
+          ...error.response,
+          message: error.response?.data?.status_message,
+        }
+        return Promise.reject(err)
+      } else {
+        return Promise.reject(error)
+      }
+    })
+}
+export { clientApi }
