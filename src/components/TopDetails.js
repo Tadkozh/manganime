3c9a159c29050247a244286377c5cfb726d874a1
Ponifty <ponifty@gmail.com>
2023-01-17 17:04:41 +0100

    🎨 Html Elements to Material UI Elements

--- src/components/TopDetails.js
+++ src/components/TopDetails.js
@@ -7,6 +7,7 @@ import { useTopOtaku } from '../hooks/queriesHooks'
 import { Container } from '@mui/material'
 import MobileStepper from '@mui/material/MobileStepper'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
 import { useTheme } from '@mui/material'
@@ -39,9 +40,9 @@ const TopDetails = ({ name, isHomePage = false }) => {
   const sxContainerHomePage = {
     maxWidth: '1600px',
     position: 'relative',
-    margin: '2em 0',
-    padding: '1em 0',
-    backgroundColor: theme.palette.background.content,
+    m: '2em 0',
+    p: '1em 0',
+    bgcolor: theme.palette.background.content,
     boxShadow:
       '0px 2px 4px 2px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)',
   }
@@ -78,9 +79,8 @@ const TopDetails = ({ name, isHomePage = false }) => {
             sx={{
               position: 'absolute',
               top: '15em',
-              // left: '0',
               width: '100%',
-              backgroundColor: 'inherit',
+              bgcolor: 'inherit',
               '.MuiMobileStepper-dots': {
                 display: 'none',
               },
@@ -93,7 +93,7 @@ const TopDetails = ({ name, isHomePage = false }) => {
                 sx={{
                   zIndex: 1,
                   color: 'rgb(68,68,68)',
-                  '&:hover': { backgroundColor: 'rgba(68,68,68,0.5)' },
+                  ':hover': { bgcolor: 'rgba(68,68,68,0.5)' },
                 }}
                 size="small"
                 onClick={filteredTopDatas.length === 4 ? handleNext : null}
@@ -107,7 +107,7 @@ const TopDetails = ({ name, isHomePage = false }) => {
                 sx={{
                   zIndex: 1,
                   color: 'rgb(68,68,68)',
-                  '&:hover': { backgroundColor: 'rgba(68,68,68,0.5)' },
+                  ':hover': { bgcolor: 'rgba(68,68,68,0.5)' },
                 }}
                 size="small"
                 onClick={handleBack}
@@ -118,7 +118,16 @@ const TopDetails = ({ name, isHomePage = false }) => {
             }
           />
         ) : null}
-        <h2>Top {name}</h2>
+        <Typography
+          variant="h4"
+          sx={{
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            marginLeft: '1em',
+          }}
+        >
+          Top {name}
+        </Typography>
         <TopView
           isHomePage={isHomePage ? true : false}
           datas={isHomePage ? filteredTopDatas : topDatas}
