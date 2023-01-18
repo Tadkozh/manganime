21ffd5625260eb33ae2344b3f3e0ed3f31a56425
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-18 22:18:06 +0400

    fix: 🐛Error Boundary mal placé

--- src/AppConsumer.js
+++ src/AppConsumer.js
@@ -16,42 +16,40 @@ import {
 } from './commons/constants'
 import { Error404 } from './components/error/Error404'
 import { ErrorFallback } from './components/error/ErrorFallBack'
+import Infos from './components/infos/Infos'
 import { LoginRegister } from './components/LoginRegister'
 import { MangAnime } from './components/MangAnime'
-
+import News from './components/news/News'
+import { PrivateRoute } from './components/PrivateRoute'
+import { ProfileUser } from './components/profile/ProfileUser'
+import Recommendations from './components/Recommendations'
 import SearchAnime from './components/search/SearchAnime'
 import SearchManga from './components/search/SearchManga'
 import TopAnime from './components/top/TopAnime'
 import TopManga from './components/top/TopManga'
 
-import Infos from './components/infos/Infos'
-
-import News from './components/news/News'
-import Recommendations from './components/Recommendations'
-import { PrivateRoute } from './components/PrivateRoute'
-import { ProfileUser } from './components/profile/ProfileUser'
-
 import MangAnimeHeader from './components/MangAnimeHeader'
 const AppConsumer = () => {
   return (
     <Router>
-      <ErrorBoundary FallbackComponent={ErrorFallback}></ErrorBoundary>
-      <MangAnimeHeader />
-      <Routes>
-        <Route path={ROUTE_HOME} element={<MangAnime />} />
-        <Route path={ROUTE_LOGIN_REGISTER} element={<LoginRegister />} />
-        <Route path={ROUTE_SEARCH_ANIME} element={<SearchAnime />} />
-        <Route path={ROUTE_SEARCH_MANGA} element={<SearchManga />} />
-        <Route path={ROUTE_TOP_ANIME} element={<TopAnime />} />
-        <Route path={ROUTE_TOP_MANGA} element={<TopManga />} />
-        <Route path={ROUTE_INFOS} element={<Infos />} />
-        <Route path={ROUTE_NEWS} element={<News />} />
-        <Route path={ROUTE_RECOMMENDATIONS} element={<Recommendations />} />
-        <Route path={ROUTE_404} element={<Error404 />} />
-        <Route path="/" element={<PrivateRoute />}>
-          <Route path={ROUTE_PROFILE} element={<ProfileUser />} />
-        </Route>
-      </Routes>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <MangAnimeHeader />
+        <Routes>
+          <Route path={ROUTE_HOME} element={<MangAnime />} />
+          <Route path={ROUTE_LOGIN_REGISTER} element={<LoginRegister />} />
+          <Route path={ROUTE_SEARCH_ANIME} element={<SearchAnime />} />
+          <Route path={ROUTE_SEARCH_MANGA} element={<SearchManga />} />
+          <Route path={ROUTE_TOP_ANIME} element={<TopAnime />} />
+          <Route path={ROUTE_TOP_MANGA} element={<TopManga />} />
+          <Route path={ROUTE_INFOS} element={<Infos />} />
+          <Route path={ROUTE_NEWS} element={<News />} />
+          <Route path={ROUTE_RECOMMENDATIONS} element={<Recommendations />} />
+          <Route path={ROUTE_404} element={<Error404 />} />
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path={ROUTE_PROFILE} element={<ProfileUser />} />
+          </Route>
+        </Routes>
+      </ErrorBoundary>
     </Router>
   )
 }
