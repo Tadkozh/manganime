7c59e42d80da00ed64b6ee0e65ff86796efc5ab5
Ponifty <ponifty@gmail.com>
2023-01-18 18:44:07 +0100

    🎨 TopView Link

--- src/components/top/TopView.js
+++ src/components/top/TopView.js
@@ -70,14 +70,7 @@ const TopView = ({ datas, isHomePage = false, type }) => {
                   alt={data?.title_english ?? data?.title}
                 />
                 {showOverlay.status && showOverlay.index === index && (
-                  <Link
-                    // to={`/collection/${
-                    //   data?.type === 'manga' ? 'manga' : 'anime'
-                    // }/search/main/${data?.mal_id}/${
-                    //   data?.title_english ?? data?.title
-                    // }`}
-                    to={getUrl(type, INFOS, [data?.mal_id])}
-                  >
+                  <Link to={getUrl(type, INFOS, [data?.mal_id])}>
                     <Box
                       component="div"
                       sx={{
