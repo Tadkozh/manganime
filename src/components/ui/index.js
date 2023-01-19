67fe2c6f0745383a38473b7e1ba227989482cc42
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-19 14:55:39 +0400

    feat: ✨Ajout de composant Mui

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,17 +1,9 @@
-import { HomeRounded, Refresh, Search, Whatshot } from '@mui/icons-material'
-import AdbIcon from '@mui/icons-material/Adb'
-import CircleIcon from '@mui/icons-material/Circle'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded'
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
-import MenuIcon from '@mui/icons-material/Menu'
 import {
   Alert,
   Backdrop,
   Card,
   CardActionArea,
+  CardActions,
   CircularProgress,
   DialogContent,
   Divider,
@@ -20,12 +12,18 @@ import {
   Pagination,
   Rating,
   Select,
-  styled,
+  Skeleton,
   Switch,
+  styled,
 } from '@mui/material'
+import { HomeRounded, Refresh, Search, Whatshot } from '@mui/icons-material'
+import { default as Item, default as Stack } from '@mui/material/Stack'
+import { blue, green, grey, red, yellow } from '@mui/material/colors'
+
 import Accordion from '@mui/material/Accordion'
 import AccordionDetails from '@mui/material/AccordionDetails'
 import AccordionSummary from '@mui/material/AccordionSummary'
+import AdbIcon from '@mui/icons-material/Adb'
 import AppBar from '@mui/material/AppBar'
 import Avatar from '@mui/material/Avatar'
 import Box from '@mui/material/Box'
@@ -33,26 +31,30 @@ import Button from '@mui/material/Button'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Checkbox from '@mui/material/Checkbox'
+import CircleIcon from '@mui/icons-material/Circle'
 import Container from '@mui/material/Container'
 import CssBaseline from '@mui/material/CssBaseline'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Grid from '@mui/material/Grid'
 import IconButton from '@mui/material/IconButton'
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
 import Link from '@mui/material/Link'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import MUISwitchMode from './MUISwitchMode'
 import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import MobileStepper from '@mui/material/MobileStepper'
 import Paper from '@mui/material/Paper'
-import { default as Item, default as Stack } from '@mui/material/Stack'
 import TextField from '@mui/material/TextField'
 import Toolbar from '@mui/material/Toolbar'
 import Tooltip from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
-import MUISwitchMode from './MUISwitchMode'
 
 export {
-  InputLabel,
-  FormControl,
   Accordion,
   AccordionSummary,
   AccordionDetails,
@@ -63,11 +65,13 @@ export {
   Alert,
   AppBar,
   Avatar,
-  Box,
+  blue,
   Backdrop,
+  Box,
   Button,
   Card,
   CardActionArea,
+  CardActions,
   CardContent,
   CardMedia,
   Checkbox,
@@ -78,10 +82,14 @@ export {
   Divider,
   DialogContent,
   FavoriteRoundedIcon,
+  FormControl,
   FormControlLabel,
+  green,
   Grid,
+  grey,
   HomeRounded,
   IconButton,
+  InputLabel,
   KeyboardArrowLeft,
   KeyboardArrowRight,
   Link,
@@ -95,13 +103,16 @@ export {
   Paper,
   Search,
   Select,
+  Skeleton,
   styled,
   Switch,
-  Refresh,
   Rating,
+  red,
+  Refresh,
   TextField,
   Toolbar,
   Tooltip,
   Typography,
+  yellow,
   Whatshot,
 }
