f7131325f7620c48aa4920527e6448e4f3dafcbd
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-25 14:39:21 +0400

    refactor: 🎨 renommage pour garder la même structure que user

--- src/components/profile/ProfileEdit.js
+++ src/components/profile/ProfileEdit.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { LOADING, SUCCESS } from '../../commons/constants'
 import { useAuth } from '../../context/AuthContext'
-import { Alert, Box, Button, LoadingScreen, TextField } from '../ui'
+import { Alert, Box, Button, Grid, LoadingScreen, TextField } from '../ui'
 
 const ProfileEdit = ({ user }) => {
   const [email, setEmail] = React.useState(user.email)
@@ -11,7 +11,7 @@ const ProfileEdit = ({ user }) => {
 
   const handdleSubmit = (event) => {
     const userUpdate = {
-      username,
+      name: username,
       email,
       password,
     }
@@ -34,35 +34,41 @@ const ProfileEdit = ({ user }) => {
   }
   if (status === SUCCESS) {
     return (
-      <Box component={'form'} onSubmit={handdleSubmit} sx={{ m: 1 }}>
-        <TextField
-          label={'Username'}
-          name={'username'}
-          autoFocus={true}
-          onChange={handleChangeUserName}
-          value={username}
-        />
-        <TextField
-          label={'Email'}
-          name={'email'}
-          autoComplete={'email'}
-          onChange={handleChangeEmail}
-          value={email}
-        />
-        <TextField
-          label={'Password'}
-          name={'password'}
-          autoComplete={'current-password'}
-          onChange={handleChangePassword}
-        />
-        <Button variant="contained" type="submit">
-          Save change
-        </Button>
-        {error ? <Alert severity="error">Erreur: {error.message}</Alert> : null}
-      </Box>
+      <Grid container>
+        <Grid item xs={12} md={6} lg={6}>
+          <Box component={'form'} onSubmit={handdleSubmit} sx={{ m: 1 }}>
+            <TextField
+              label={'Username'}
+              name={'username'}
+              autoFocus={true}
+              onChange={handleChangeUserName}
+              value={username}
+            />
+            <TextField
+              label={'Email'}
+              name={'email'}
+              autoComplete={'email'}
+              onChange={handleChangeEmail}
+              value={email}
+            />
+            <TextField
+              label={'Password'}
+              name={'password'}
+              autoComplete={'current-password'}
+              onChange={handleChangePassword}
+            />
+            <Button variant="contained" type="submit">
+              Save change
+            </Button>
+            {error ? (
+              <Alert severity="error">Erreur: {error.message}</Alert>
+            ) : null}
+          </Box>
+        </Grid>
+        <Grid item xs={6}></Grid>
+      </Grid>
     )
   }
 }
 
 export { ProfileEdit }
-
