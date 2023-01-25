0d0fc12f30dfc0a87e2b7d9732a7fa7ada447a63
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-25 14:32:21 +0400

    refactor/feat: 🎨✨ Déplacement, renommage et refactorisation des fonctions et centralisation des actions sur un utilisateur dans la BDD

---
+++ src/database/user.js
@@ -0,0 +1,54 @@
+import { getUid } from '../firebase-config'
+import { addUser, getUserById, updateUser } from './operations'
+
+const updateProfileUser = (user, userCurrent) => {
+  const newUser = structuredClone(userCurrent)
+  newUser.email = user.email
+  newUser.password = user.password
+  newUser.name = user.name
+  return newUser
+}
+const updateRanking = (type, info, value, user) => {
+  const newUser = structuredClone(user)
+  const type_opinion = type === 'anime' ? 'anime_opinion' : 'manga_opinion'
+  const type_id = type === 'anime' ? 'anime_id' : 'manga_id'
+
+  const isAnimeId = newUser[type_opinion].some(
+    (opinion) => opinion.anime_id === info.mal_id,
+  )
+  if (isAnimeId) {
+    newUser[type_opinion].foreach((opinion) => {
+      if (opinion.anime_id === info.mal_id) {
+        opinion.rate = value
+      }
+    })
+  } else {
+    const ratedb = { rate: value, [type_id]: info.mal_id }
+    newUser[type_opinion].push(ratedb)
+  }
+  updateUserCurrent(newUser)
+}
+
+const storeUser = (data) => {
+  if (data != null) {
+    const uid = getUid()
+    addUser(uid, data.user)
+  }
+}
+const updateUserCurrent = async (newUser) => {
+  const uid = getUid()
+  await updateUser(uid, newUser)
+}
+
+const getUser = async (currentUser) => {
+  const user = await getUserById(currentUser.uid)
+  return user
+}
+
+export {
+  updateProfileUser,
+  updateUserCurrent,
+  updateRanking,
+  getUser,
+  storeUser,
+}
