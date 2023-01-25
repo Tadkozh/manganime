5b5417af0ed7be2f24ff76a8947244e5e684f53e
Dimitri Lhopital <dimitrilho.gmail.com>
2023-01-25 11:11:22 +0400

    refactor: 🎨Utilisation de la nouvelle fonction, mise en place de chargement lors de la mise à jour de l'utilisateur

--- src/components/profile/ProfileEdit.js
+++ src/components/profile/ProfileEdit.js
@@ -1,17 +1,22 @@
-import { Alert, Box, Button, TextField } from '../ui'
 import React from 'react'
-import { SIGN_UPDATE } from '../../commons/constants'
+import { LOADING, SUCCESS } from '../../commons/constants'
 import { useAuth } from '../../context/AuthContext'
+import { Alert, Box, Button, LoadingScreen, TextField } from '../ui'
 
 const ProfileEdit = ({ user }) => {
   const [email, setEmail] = React.useState(user.email)
   const [password, setPassword] = React.useState('')
   const [username, setUsername] = React.useState(user.name)
-  const { preValidate, error } = useAuth()
+  const { validationProfile, error, data: currentUser, status } = useAuth()
 
   const handdleSubmit = (event) => {
+    const userUpdate = {
+      username,
+      email,
+      password,
+    }
     event.preventDefault()
-    preValidate(email, password, SIGN_UPDATE, true)
+    validationProfile(userUpdate, currentUser)
   }
 
   const handleChangeUserName = (e) => {
@@ -24,34 +29,40 @@ const ProfileEdit = ({ user }) => {
     setPassword(e.target.value)
   }
 
-  return (
-    <Box component={'form'} onSubmit={handdleSubmit} sx={{ m: 1 }}>
-      <TextField
-        label={'Username'}
-        name={'username'}
-        autoFocus={true}
-        onChange={handleChangeUserName}
-        value={username}
-      />
-      <TextField
-        label={'Email'}
-        name={'email'}
-        autoComplete={'email'}
-        onChange={handleChangeEmail}
-        value={email}
-      />
-      <TextField
-        label={'Password'}
-        name={'password'}
-        autoComplete={'current-password'}
-        onChange={handleChangePassword}
-      />
-      <Button variant="contained" type="submit">
-        Save change
-      </Button>
-      {error ? <Alert severity="error">Erreur: {error.message}</Alert> : null}
-    </Box>
-  )
+  if (status === LOADING) {
+    return <LoadingScreen />
+  }
+  if (status === SUCCESS) {
+    return (
+      <Box component={'form'} onSubmit={handdleSubmit} sx={{ m: 1 }}>
+        <TextField
+          label={'Username'}
+          name={'username'}
+          autoFocus={true}
+          onChange={handleChangeUserName}
+          value={username}
+        />
+        <TextField
+          label={'Email'}
+          name={'email'}
+          autoComplete={'email'}
+          onChange={handleChangeEmail}
+          value={email}
+        />
+        <TextField
+          label={'Password'}
+          name={'password'}
+          autoComplete={'current-password'}
+          onChange={handleChangePassword}
+        />
+        <Button variant="contained" type="submit">
+          Save change
+        </Button>
+        {error ? <Alert severity="error">Erreur: {error.message}</Alert> : null}
+      </Box>
+    )
+  }
 }
 
 export { ProfileEdit }
+
