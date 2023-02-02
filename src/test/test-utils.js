3fe0e2c118886d3392889fefe4c4914eaeb6bf86
Dimitri Lhopital <dimitrilho.gmail.com>
2023-02-02 18:05:23 +0400

    test: ✅ Mise en place du router dans le wrapper

--- src/test/test-utils.js
+++ src/test/test-utils.js
@@ -1,13 +1,17 @@
 import { render as renderReactTestingLib } from '@testing-library/react'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { AppProviders } from '../context/index'
 
 const wrapperContext = ({ children }) => {
-  return <AppProviders>{children}</AppProviders>
+  return (
+    <AppProviders>
+      <BrowserRouter>{children}</BrowserRouter>
+    </AppProviders>
+  )
 }
-
 const render = (ui, { ...options } = {}) => {
-  return renderReactTestingLib(ui, { wrapper: AppProviders, ...options })
+  return renderReactTestingLib(ui, { wrapper: wrapperContext, ...options })
 }
 
 export * from '@testing-library/react'
